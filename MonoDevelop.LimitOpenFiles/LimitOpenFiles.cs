c93b0367f7b9c2cb9be002a6f9e115c33985f383
Mathias Baumann <mathias@labs03.(none)>
2012-10-08 18:53:32 +0200

    initial commit

---
+++ MonoDevelop.LimitOpenFiles/LimitOpenFiles.cs
@@ -0,0 +1,53 @@
+using MonoDevelop.Components.Commands;
+
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
+
+using MonoDevelop.Core;
+
+namespace MonoDevelop.LimitOpenFiles
+{
+    public enum Commands
+    {
+        LimitOpenfiles,
+    }
+
+    class LimitOpenFiles : CommandHandler
+    {
+        protected override void Run ()
+        {
+            base.Run();
+            IdeApp.Workbench.DocumentOpened += HandleDocumentOpened;
+        }
+            
+        protected override void Update ( CommandInfo info )
+        {
+        }
+
+
+        protected void HandleDocumentOpened ( object sender, MonoDevelop.Ide.Gui.DocumentEventArgs e )
+        { 
+            if ( IdeApp.Workbench.Documents.Count > 10 )
+            {          
+                for ( int i = 0; 
+                      i < IdeApp.Workbench.Documents.Count ;
+                      ++i )  
+                    if ( IdeApp.Workbench.Documents[i].IsDirty == false &&
+                         object.ReferenceEquals(IdeApp.Workbench.ActiveDocument,IdeApp.Workbench.Documents[i]) == false && 
+                         object.ReferenceEquals(e.Document,IdeApp.Workbench.Documents[i]) == false )
+                    {
+                        IdeApp.Workbench.Documents[i].Close();
+                        break;
+                    }
+
+            }
+        }
+    
+
+        public LimitOpenFiles ()
+        {
+
+        }
+    }
+}
+
